57f86e87675375156e03b2859fb617f7deb3001c
GeorgeMBS <ilustrageorge@gmail.com>
2023-02-18 13:07:53 -0300

    Menu estilo Lava Lamp
    Implementação do menu estilo lava lamp.

--- hamburgueria/src/App.jsx
+++ hamburgueria/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Menu from './components/menu/Menu'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Menu />
     </div>
   )
 }
---
+++ hamburgueria/src/components/menu/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Menu.css'
+
+const Menu = () => {
+  return (
+    <div>
+      <nav className='Nav'>
+        <a href="#">Home</a>
+        <a href="#">Cardápio</a>
+        <a href="#">filiais</a>
+        <a href="#">contato</a>
+        <a href="#">sobre nós</a>
+        <div className="animation start-home">
+        </div>
+      </nav>
+
+    </div>
+  )
+}
+
+export default Menu
