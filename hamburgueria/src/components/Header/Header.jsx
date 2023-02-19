aef0bbfb7d6d20b005e458a4ae6f008947648987
GeorgeMBS <ilustrageorge@gmail.com>
2023-02-18 23:11:26 -0300

    Estilos
    Correção no flexbox dos estilos do header e do carrinho de compras.

--- hamburgueria/src/components/Header/Header.jsx
+++ hamburgueria/src/components/Header/Header.jsx
@@ -5,15 +5,15 @@ import ShoppingCart from './ShoppingCart'
 const Header = () => {
   return (
     <div className='container'>
-      <div className='logoContainer'>
-        <div className='logo'>
-          <img src="../src/images/hamburguer.png" alt="logo" />
-        </div>
-        <div className='title'>
-          <h1>Hamburgueria</h1>
-        </div>
+      <div className='logo'>
+        <img src="../src/images/carne.png" alt="logo" />
       </div>
-      <ShoppingCart />
+      <div className='title'>
+        <h1>Hamburgueria</h1>
+      </div>
+      <div className='Cart'>
+        <ShoppingCart />
+      </div>
     </div>
   )
 }
--- hamburgueria/src/components/Header/ShoppingCart.jsx
+++ hamburgueria/src/components/Header/ShoppingCart.jsx
@@ -3,12 +3,11 @@ import styles from './ShoppingCart.css'
 
 export const ShoppingCart = () => {
   return (
-    <div className='cartAll'>
-      <div className='cartBg'>
-        <div>
-          <img className='cartLogo' src="../src/images/cart.png" alt="cart" />
-        </div>
+    <div className='cartBg'>
+      <div>
+        <img className='cartLogo' src="../src/images/cart.png" alt="cart" />
       </div>
+
     </div>
   )
 }
