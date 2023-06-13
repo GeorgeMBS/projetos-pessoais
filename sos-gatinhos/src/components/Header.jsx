ebdd8402900ec8523740e18fdc5701baba7553c3
GeorgeMBS <99207070+GeorgeMBS@users.noreply.github.com>
2023-06-13 17:42:03 -0300

    Atualização header

--- sos-gatinhos/src/components/Header.jsx
+++ sos-gatinhos/src/components/Header.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <div><img src="" alt="" /></div>
+      <div><img src="\imagens\Logo1.png" alt="Logotipo" title='Logotipo Gatos da Colônia' /></div>
       <h1>SOS Gatos da Colônia</h1>
       <ul>
-        <li>teste</li>
-        <li>teste</li>
-        <li>teste</li>
-        <li>teste</li>
-        <li>teste</li>
+        <li>Home</li>
+        <li>Sobre nós</li>
+        <li>Contato</li>
+        <li>Brechó</li>
+        <li>Doações</li>
       </ul>
 
     </div>
