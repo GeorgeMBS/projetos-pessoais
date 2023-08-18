0127d88198b360dbeb680ec9243541c9cee877ab
GeorgeMBS <99207070+GeorgeMBS@users.noreply.github.com>
2023-08-18 15:22:49 -0300

    home-slider-updt
    Agora foi :)

--- sos-gatinhos/src/components/Home.jsx
+++ sos-gatinhos/src/components/Home.jsx
@@ -1,45 +1,58 @@
 import React from 'react'
 import './styles/Home.css'
-import AwesomeSlider from 'react-awesome-slider'
 
+import { motion } from 'framer-motion'
 
+import Adotados1 from '/src/Adotados01.jpg'
+import Adotados2 from '/src/Adotados02.jpg'
+import Adotados4 from '/src/Adotados04.jpg'
+import Adotados5 from '/src/Adotados05.jpg'
+import Adotados6 from '/src/Adotados06.jpg'
+import Adotados7 from '/src/Adotados07.jpg'
+import adote from '/src/adoteumgato.jpg'
+
+import { useState, useEffect, useRef } from 'react'
+
+const Adotados = [adote, Adotados1, Adotados2, Adotados4, Adotados5, Adotados6, Adotados7, adote]
 
 const Home = () => {
 
+   const carousel = useRef();
+   const [width, setWidth] = useState(0)
 
+   useEffect(() => {
+      console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth)
+      setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+
+   }, [])
 
 
   return (
 
-
     <div className='principal'>
+        <motion.div ref={carousel} className='carousel' whileTap={{cursor:"grabbing"}}>
+          <motion.div 
+          className='inner' 
+          drag="x" 
+          dragConstraints={{ right: 0, left: -width }}
+          initial={{ x: 300}}
+          animate={{ x: 0}}
+          transition={{ duration: 2 }}
+          >
+            
+            {Adotados.map(image => (
+              <motion.div className='item' key={image}>
+                <img src={image} alt="carrossel" />
+              </motion.div>
+            ))}
 
+          </motion.div>
+        </motion.div>
+      
 
-      
-        <div className="slider">
-          <AwesomeSlider>
-            <div data-src="/Adotados01.jpg" />
-            <div data-src="/Adotados02.jpg" />
-            <div data-src="/Adotados04.jpg" />
-            <div data-src="/Adotados05.jpg" />
-            <div data-src="/Adotados06.jpg" />
-            <div data-src="/Adotados07.jpg" />
-          </AwesomeSlider>
-        </div>
-      
-      <div className="texto">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ad recusandae rerum doloremque esse necessitatibus sapiente repudiandae, maxime hic mollitia quis quae sed at molestiae quam voluptate illum praesentium dolores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsa voluptatem accusamus accusantium dignissimos ut incidunt at in sed quam! Fugit nisi reiciendis eos nam unde totam ratione enim eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sequi porro eos! Odio, accusantium? Ab, magni dicta cumque molestias quibusdam aliquam. Aut mollitia iusto veritatis enim dolores modi ipsa asperiores.</p>
-      </div>
-      
-      <div className="texto">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ad recusandae rerum doloremque esse necessitatibus sapiente repudiandae, maxime hic mollitia quis quae sed at molestiae quam voluptate illum praesentium dolores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsa voluptatem accusamus accusantium dignissimos ut incidunt at in sed quam! Fugit nisi reiciendis eos nam unde totam ratione enim eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sequi porro eos! Odio, accusantium? Ab, magni dicta cumque molestias quibusdam aliquam. Aut mollitia iusto veritatis enim dolores modi ipsa asperiores.</p>
-      </div>
-
-      <div className="texto">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ad recusandae rerum doloremque esse necessitatibus sapiente repudiandae, maxime hic mollitia quis quae sed at molestiae quam voluptate illum praesentium dolores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsa voluptatem accusamus accusantium dignissimos ut incidunt at in sed quam! Fugit nisi reiciendis eos nam unde totam ratione enim eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sequi porro eos! Odio, accusantium? Ab, magni dicta cumque molestias quibusdam aliquam. Aut mollitia iusto veritatis enim dolores modi ipsa asperiores.</p>
-      </div>
 
     </div>
+
 
 
 
@@ -48,3 +61,4 @@ const Home = () => {
 }
 
 export default Home
+
